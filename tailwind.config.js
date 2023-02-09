ff93d1a045650913ce4e15d4ff5cb8fa41a71325
Ruchir <ruchirdandge7020@gmail.com>
2023-02-09 18:30:10 +0530

    Added Menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,10 @@ module.exports = {
     },
     extend: {
       colors: {
-        brightRed: 'hsl (12, 88%, 59%)',
+        brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 888, 95%)', 
-        darkBlue: 'hsl (228, 39%, 23%)', 
+        darkBlue: 'hsl(228, 39%, 23%)', 
         darkGrayishBlue: 'hs1(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%',
